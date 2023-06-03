3311c6fa3acfbdb3699d1af3eb7dd319739081f4
Ritesh18117 <rgriteshbyahut18117rg@gmail.com>
2023-06-03 13:14:59 +0530

    IP config

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-undef
-const socket = io.connect('http://localhost:3000');
+const socket = io.connect('https://chat-me-coral.vercel.app:3000');
 
 const message = document.getElementById('message-input');
 const sendMsg = document.getElementById('send-message');
